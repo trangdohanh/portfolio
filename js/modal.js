2f1e0f09ac117c612ce988e9a6c1b22ab3c8285e
trangdohanh <125163105+trangdohanh@users.noreply.github.com>
2025-05-26 21:47:54 -0400

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,9 @@
 function openModal(e) {
   if (e.className == "video-modal"){
-    document.getElementById("video-modal").src = `img/lightbox/${e.id}.mp4`;
+    document.getElementById("video-modal").src = `../img/lightbox/${e.id}.mp4`;
     document.getElementById("video-modal").style.display = "block";
   } else{
-    document.getElementById("img-modal").src = `img/lightbox/${e.id}.png`;
+    document.getElementById("img-modal").src = `../img/lightbox/${e.id}.png`;
     document.getElementById("img-modal").style.display = "block";
   }
   
