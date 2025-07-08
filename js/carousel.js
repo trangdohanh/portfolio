9296ac6ca59442677a781b3ca056888f688db8b3
trangdohanh <125163105+trangdohanh@users.noreply.github.com>
2025-07-08 19:27:19 -0400

    Add files via upload

---
+++ js/carousel.js
@@ -0,0 +1,21 @@
+// Start of code from https://www.w3schools.com/howto/howto_js_slideshow_gallery.asp
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {showSlides(slideIndex += n);}
+function currentSlide(n) {showSlides(slideIndex = n);}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let thumbnails = document.getElementsByClassName("thumbnail");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].className = thumbnails[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  thumbnails[slideIndex-1].className += " active";
+}
+// End of code from https://www.w3schools.com/howto/howto_js_slideshow_gallery.asp
