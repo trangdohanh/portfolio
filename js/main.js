c405ace6c621bc3683fd41f293bc3ba55493f716
trangdohanh <125163105+trangdohanh@users.noreply.github.com>
2025-08-06 12:18:05 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,3 +21,33 @@ document.getElementById("nav-trigger").addEventListener("click", () => {
     document.getElementById("nav-triggered").classList.toggle("hide-nav");
     document.getElementById("nav-triggered").classList.toggle("show-nav");
 })
+
+
+var pageNav = document.getElementById("page-nav")
+if (document.body.contains(pageNav)){
+    // Code from https://www.youtube.com/watch?v=nwCtWn-xFz0
+    const navLinks = document.querySelectorAll('.nav-link');
+    const sectionElements = document.querySelectorAll('.section');
+    let currentSection = 'overview';
+
+    window.addEventListener('scroll', () => {
+        sectionElements.forEach(sectionElement => {
+            if (window.scrollY >= sectionElement.offsetTop - 300) {
+                currentSection = sectionElement.id;
+            }
+        });
+
+        navLinks.forEach(navLink => {
+            if (navLink.href.includes(currentSection)){
+                document.querySelector('.nav-link-active').classList.remove('nav-link-active');
+                navLink.classList.add('nav-link-active');
+            };
+        });
+
+        if (scrollY >= document.querySelector('footer').offsetTop - document.querySelector('footer').clientHeight*4.8){
+            document.querySelector('.page-nav-container').style.display = "none";
+        } else {
+            document.querySelector('.page-nav-container').style.display = "flex";
+        }
+    })
+}
