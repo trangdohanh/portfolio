9b99463091c98cdb796205e34f41c530e62f1202
trangdohanh <125163105+trangdohanh@users.noreply.github.com>
2025-09-12 22:39:30 -0400

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,23 @@
+var template = document.createElement("div");
+template.setAttribute("class", "modal");
+template.innerHTML = `<div class="close-overlay" onclick="closeModal()"></div>
+                      <span class="material-icons close-modal" onclick="closeModal()">close</span>
+                      <div class="modal-content" id="modal-content"></div>`
+
 function openModal(e) {
+  document.body.appendChild(template)
+  template.style.display = "flex";
   if (e.classList.contains("video-modal")){
-    document.getElementById("video-modal").src = `img/lightbox/${e.id}.mp4`;
-    document.getElementById("video-modal").style.display = "block";
+    document.getElementById("modal-content").innerHTML = `<video width="90%" height="auto" id="video-modal" controls>
+                                                              <source type="video/mp4" src="../img/lightbox/${e.id}.mp4">
+                                                          </video>`;
   } else{
-    document.getElementById("img-modal").src = `img/lightbox/${e.id}.png`;
-    document.getElementById("img-modal").style.display = "block";
+    document.getElementById("modal-content").innerHTML = `<img id="img-modal" src="../img/lightbox/${e.id}.png">`
   }
-  
-  document.getElementById("modal").style.display = "flex";
 }
 
 function closeModal(){
-  document.getElementById("modal").style.display = "none";
-  document.getElementById("img-modal").style.display = "none";
-  document.getElementById("video-modal").style.display = "none";
-  document.getElementById("video-modal").pause();
+  template.style.display = "none";
+  document.getElementById("modal-content").innerHTML = ``;
+  document.body.removeChild(template)
 }
