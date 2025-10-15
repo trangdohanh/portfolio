5a4ff9a762e04274fd9d566c6bf17b40209755c5
trangdohanh <125163105+trangdohanh@users.noreply.github.com>
2025-10-14 22:16:51 -0400

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -6,16 +6,19 @@ function currentSlide(n) {showSlides(slideIndex = n);}
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
-  let thumbnails = document.getElementsByClassName("thumbnail");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < thumbnails.length; i++) {
-    thumbnails[i].className = thumbnails[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  thumbnails[slideIndex-1].className += " active";
+  
+  let thumbnails = document.getElementsByClassName("thumbnail");
+  if (thumbnails == true){
+    for (i = 0; i < thumbnails.length; i++) {
+      thumbnails[i].className = thumbnails[i].className.replace(" active", "");
+    }
+    thumbnails[slideIndex-1].className += " active";
+  }
 }
 // End of code from https://www.w3schools.com/howto/howto_js_slideshow_gallery.asp
