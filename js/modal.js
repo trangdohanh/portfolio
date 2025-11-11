224860daf3999f03f262463103f805ff50f74a25
trangdohanh <125163105+trangdohanh@users.noreply.github.com>
2025-11-11 13:53:05 -0500

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,9 @@
 var template = document.createElement("div");
 template.setAttribute("class", "modal");
 template.innerHTML = `<div class="close-overlay" onclick="closeModal()"></div>
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" style="width: 1.5rem; height: 1.5rem;" class="close-modal" onclick="closeModal()><path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path></svg>
+                      <span class="close-modal" onclick="closeModal()">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" style="width: 1.5rem; height: 1.5rem;"><path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path></svg>
+                      </span>
                       <div class="modal-content" id="modal-content"></div>`
 
 function openModal(e) {
