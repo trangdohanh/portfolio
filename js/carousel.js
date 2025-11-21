d1836c631569b00a9df7f8a48e9a95c3eea487b4
trangdohanh <125163105+trangdohanh@users.noreply.github.com>
2025-11-21 13:59:32 -0500

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,11 +1,14 @@
 // Start of code from https://www.w3schools.com/howto/howto_js_slideshow_gallery.asp
 let slideIndex = 1;
-showSlides(slideIndex);
+
 function plusSlides(n) {showSlides(slideIndex += n);}
 function currentSlide(n) {showSlides(slideIndex = n);}
+
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
+  let thumbnails = document.getElementsByClassName("thumbnail");
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -13,12 +16,13 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   
-  let thumbnails = document.getElementsByClassName("thumbnail");
-  if (thumbnails == true){
+  if (thumbnails){
     for (i = 0; i < thumbnails.length; i++) {
       thumbnails[i].className = thumbnails[i].className.replace(" active", "");
     }
     thumbnails[slideIndex-1].className += " active";
   }
 }
+
+showSlides(slideIndex);
 // End of code from https://www.w3schools.com/howto/howto_js_slideshow_gallery.asp
