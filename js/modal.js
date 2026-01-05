d6471df08605d4401a74a94ad21506db28d47890
trangdohanh <125163105+trangdohanh@users.noreply.github.com>
2026-01-05 00:15:50 -0500

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -14,8 +14,14 @@ function openModal(e) {
                                                               <source type="video/mp4" src="../img/lightbox/${e.id}.mp4">
                                                           </video>`;
   } else if (e.classList.contains("gif-modal")){
-    document.getElementById("modal-content").innerHTML = `<a href="#works" onclick="closeModal()"><img id="img-modal" src="../img/${e.id}.gif"><a>`
-  } else{
+    document.getElementById("modal-content").innerHTML = `<a href="#works" onclick="closeModal()"><img id="img-modal" src="../img/animation.gif"><a>`
+  } else if (e.classList.contains("link-modal")){
+    document.getElementById("modal-content").innerHTML = `<div class="content-flex">
+                                                            <iframe style="aspect-ratio: 16/9;" src="https://www.youtube.com/embed/CFDjF0xTOzE?si=MxlUWQ0FFZkbo3_I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                                            <p>Career Courier is a web platform designed to help high school students in Ontario make informed decisions about their paths after graduation. It provides important information and necessary resources in a structured and well-paced manner. Instead of suggesting programs and schools based solely on the student's interests, we aim to present all the appropriate career and training options.</p>
+                                                            <p>This project was completed for Hackville 2024 in collaboration with <a href="https://www.linkedin.com/in/adriana-baric/" target="_blank" class="link">Adriana Baric</a>, <a href="https://www.linkedin.com/in/euna-lim/" target="_blank" class="link">Euna Lim</a>, and <a href="https://www.linkedin.com/in/kelly-kou/" target="_blank" class="link">Kelly Kou</a>. View this project on <a href="https://devpost.com/software/career-courier" target="_blank" class="link">Devpost</a></p>
+                                                          </div>`
+  } else {
     document.getElementById("modal-content").innerHTML = `<img id="img-modal" src="../img/lightbox/${e.id}.png">`
   }
 }
