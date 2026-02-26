9061242ba7dbbbac6b0526f8178d24b632c42e42
trangdohanh <125163105+trangdohanh@users.noreply.github.com>
2026-02-25 22:38:41 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,12 +10,13 @@ if (!document.body.classList.contains("fixed-bg")){
                                                                 Trang Do
                                                             </a>
                                             
-                                                            <span class="nav" id="nav-trigger">
+                                                            <button class="icon-btn nav" id="nav-trigger">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" style="width: 1.5rem; height: 1.5rem;"><path d="M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128ZM40,72H216a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16ZM216,184H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z"></path></svg>
-                                                            </span>
-                                                            <span class="nav hide-trigger" id="nav-close">
+                                                            </button>
+                                                            
+                                                            <button class="icon-btn nav hide-trigger" id="nav-close">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" style="width: 1.5rem; height: 1.5rem;"><path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path></svg>
-                                                            </span>
+                                                            </button>
                                                         </div>
                                                         
                                                         <nav id="nav-triggered">
@@ -40,19 +41,45 @@ document.getElementById("footer").innerHTML = `<div>
                                                     </div>
                                                 </div>
                                                 
-                                                <span class="label theme">Made with HTML, CSS, and JS</span>`;
+                                                <span class="label">Made with HTML, CSS, and JS</span>`;
+
+
+// Fade in animation
+// Code from https://www.youtube.com/watch?v=T33NN_pPeNI
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("fadeIn");
+        } else {
+            if (!entry.target.classList.contains("animatedIn")) {
+                entry.target.classList.remove("fadeIn");
+            }
+        }
+    }) 
+}, {rootMargin: "0px 0px -10% 0px"})
+
+document.querySelectorAll([".animated", ".animatedIn"]).forEach((el) => observer.observe(el));
+// End of code from https://www.youtube.com/watch?v=T33NN_pPeNI
+
 
 var navbar = document.getElementById("nav-triggered")
 var navOpen = document.getElementById("nav-trigger");
 var navClose = document.getElementById("nav-close");
+var main = document.querySelector("main")
+var footer = document.querySelector("footer")
+
 var tabcontent = document.getElementsByClassName("tabcontent");
 var tablinks = document.getElementsByClassName("tablinks");
+
 // Code from https://www.w3schools.com/howto/howto_js_media_queries.asp
 function checkDevice(x) {
     navbar.classList.remove("hide-nav");
     navOpen.classList.remove("hide-trigger");
     navClose.classList.add("hide-trigger");
-    if (x.matches) {navbar.classList.add("hide-nav");}
+    if (x.matches) {
+        navbar.classList.add("hide-nav");
+        navbar.inert = true;
+    }
 
     if (document.body.contains(tablinks[0])) {
         tablinks[0].setAttribute("id", "defaultOpen")
@@ -74,12 +101,18 @@ navOpen.addEventListener("click", () => {
     navbar.classList.remove("hide-nav");
     navOpen.classList.add("hide-trigger");
     navClose.classList.remove("hide-trigger");
+    navbar.inert = false;
+    main.inert = true;
+    footer.inert = true;
 })
 
 navClose.addEventListener("click", () => {
     navbar.classList.add("hide-nav");
     navOpen.classList.remove("hide-trigger");
     navClose.classList.add("hide-trigger");
+    navbar.inert = true;
+    main.inert = false;
+    footer.inert = false;
 })
 
 function openDevice(evt, deviceName) {
@@ -90,6 +123,7 @@ function openDevice(evt, deviceName) {
 }
 
 
+// In-page navigation
 var pageNav = document.getElementById("page-nav")
 if (document.body.contains(pageNav)){
     // Code from https://www.youtube.com/watch?v=nwCtWn-xFz0
@@ -119,8 +153,10 @@ if (document.body.contains(pageNav)){
     })
     // End of code from https://www.youtube.com/watch?v=nwCtWn-xFz0
 }
+//////////
 
 
+// Light mode toggle
 var lightToggle = document.getElementById("switch-input")
 if (document.body.contains(lightToggle)){
     var setDarkMode;
@@ -145,7 +181,7 @@ if (document.body.contains(lightToggle)){
         }
     }
     checkSaved();
-    
+
     document.querySelector(".switch").addEventListener("mouseover", () => {
         document.querySelector(".inactive-mode").style.scale = "120%";
         document.querySelector(".inactive-mode").style.opacity = "0.8";
@@ -174,9 +210,95 @@ if (document.body.contains(lightToggle)){
         lightMode.classList.toggle("inactive-mode")
     })
 }
+//////////
 
 
-/* Code from https://codepen.io/pig3onkick3r/pen/YzqqWKY */
+// Modal
+// Code from https://www.geeksforgeeks.org/javascript/read-json-file-using-javascript/
+var content;
+function fetchJSONData() {
+    fetch('../files/content.json')
+        .then(response => {
+            if (!response.ok) {throw new Error(`HTTP error! Status: ${response.status}`);}
+            return response.json();  
+        })
+        .then(data => {
+            content = data;
+            return content
+        })  
+        .catch(error => console.error('Failed to fetch data:', error)); 
+}
+fetchJSONData();
+// End of code from https://www.geeksforgeeks.org/javascript/read-json-file-using-javascript/
+
+var template = document.createElement("div");
+template.setAttribute("class", "modal");
+template.innerHTML = `<div class="close-overlay" onclick="closeModal()"></div>
+                      <button class="icon-btn close-modal" onclick="closeModal()">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" style="width: 1.5rem; height: 1.5rem;"><path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path></svg>
+                      </button>
+                      <div class="modal-content" id="modal-content"></div>`
+
+function openModal(e) {
+	document.body.appendChild(template)
+	template.style.display = "flex";
+	var modal = template.querySelector("#modal-content")
+	if (e.classList.contains("video-modal")){
+		modal.innerHTML = `<video width="100%" height="auto" id="video-modal" controls>
+								<source type="video/mp4" src="../img/lightbox/${e.id}.mp4">
+							</video>`;
+	} else if (e.classList.contains("proj-modal")){
+		modal.innerHTML = `<img id="img-modal" src="${e.querySelector("img").src}">`
+	} else if (e.classList.contains("page-modal")){
+        template.classList.add("page")
+        if (e.id == "career-courier") {
+            modal.innerHTML = `<div class="grid row-sm">
+                                    <iframe style="aspect-ratio: 16/9;" src="${content.hackathon.youtube}" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                    <div>
+                                        <p>${content.hackathon.paragraph_1}</p>
+                                        <p>This was an entry for Hackville 2024, in collaboration with <a href="${content.hackathon.designer_1}" target="_blank" class="link">Adriana Baric</a>, <a href="${content.hackathon.designer_2}" target="_blank" class="link">Euna Lim</a>, and <a href="${content.hackathon.designer_3}" target="_blank" class="link">Kelly Kou</a>. You can view this project on <a href="${content.hackathon.link}" target="_blank" class="link">Devpost</a>.</p>
+                                    </div>
+                                </div>`
+		}
+	} else{
+		modal.innerHTML = `<img id="img-modal" src="../img/lightbox/${e.id}.png">`
+	}
+}
+
+function closeModal(){
+	template.style.display = "none";
+	if (template.classList.contains("page")) {template.classList.remove("page")}
+	document.getElementById("modal-content").innerHTML = ``;
+	document.body.removeChild(template)
+}
+
+footer.querySelector("span.label").addEventListener("click", () => {
+    document.body.appendChild(template)
+	template.style.display = "flex";
+	template.classList.add("page")
+    template.querySelector("#modal-content").innerHTML = 
+        `<div class="grid row-xs">
+            <h2>${content.website.heading}</h2>
+            <p>${content.website.paragraph_1}
+                    <svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg" style="width: 1.5rem; height: 1.5rem; fill: var(--black) !important; vertical-align: top;"><path d="M80,56V24a8,8,0,0,1,16,0V56a8,8,0,0,1-16,0Zm40,8a8,8,0,0,0,8-8V24a8,8,0,0,0-16,0V56A8,8,0,0,0,120,64Zm32,0a8,8,0,0,0,8-8V24a8,8,0,0,0-16,0V56A8,8,0,0,0,152,64Zm96,56v8a40,40,0,0,1-37.51,39.91,96.59,96.59,0,0,1-27,40.09H208a8,8,0,0,1,0,16H32a8,8,0,0,1,0-16H56.54A96.3,96.3,0,0,1,24,136V88a8,8,0,0,1,8-8H208A40,40,0,0,1,248,120ZM200,96H40v40a80.27,80.27,0,0,0,45.12,72h69.76A80.27,80.27,0,0,0,200,136Zm32,24a24,24,0,0,0-16-22.62V136a95.78,95.78,0,0,1-1.2,15A24,24,0,0,0,232,128Z"></path></svg>
+            </p>
+            <p>${content.website.paragraph_2}</p>
+            <p>${content.website.paragraph_3}</p>
+            <p>${content.website.paragraph_4}</p>
+            <p>${content.website.paragraph_5}</p>
+            <p>${content.website.paragraph_6}</p>
+            <svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg" style="width: 1.5rem; height: 1.5rem; fill: var(--green) !important; margin-top: var(--text-space-100);">
+                <path d="M128 0C198.692 0 256 57.3076 256 128C256 198.692 198.692 256 128 256H112V224C112 206.603 98.1176 192.449 80.8262 192.011L80 192H76.001C69.4768 192 64.1681 197.207 64.0039 203.691L64 204.001V244.044C63.9756 250.651 58.6124 255.999 52 255.999H64V256H32V255.998H18V253.551C26.0145 251.731 31.9971 244.565 31.998 236H32V76C32.0002 69.3726 37.3736 64.0003 44.001 64H77.9971V66.4473C70.0076 68.2611 64.0381 75.3885 64 83.918V96H64.0029C64.2565 131.381 99.9744 160 144 160C188.183 160 224 131.177 224 95.6211C224 74.298 211.118 55.3966 191.275 43.6807C172.658 56.0673 160.309 77.1295 160.006 101.096L160 102V101C160 109.837 152.837 117 144 117C135.302 117 128.224 110.059 128.005 101.413L128 101V95H127.991C127.461 60.3902 99.459 32.4438 64.8271 32.0049L64 32H19.999L19.5986 32.0039C11.211 32.1872 4.23749 38.1084 2.44629 45.998H0.000976562V32H0V9.72363C0.871806 4.77339 4.77827 0.86943 9.72949 0H128Z"/>
+                <ellipse cx="192" cy="94" rx="14" ry="14"/>
+                <ellipse cx="96" cy="94" rx="14" ry="14"/>
+            </svg>
+        </div>`
+})
+//////////
+
+
+// Before-after slider
+// Code from https://codepen.io/pig3onkick3r/pen/YzqqWKY
 const sliders = document.getElementsByClassName('before-after-slider');
 if (document.body.contains(sliders[0])){
     Array.from(sliders).forEach((slider) => {
@@ -233,35 +355,20 @@ if (document.body.contains(sliders[0])){
             e.returnValue=false;
             return false;
         }
-        /* End of code from https://codepen.io/pig3onkick3r/pen/YzqqWKY */
+        // End of code from https://codepen.io/pig3onkick3r/pen/YzqqWKY
     })
 }
+//////////
 
 
-// Code from https://www.youtube.com/watch?v=T33NN_pPeNI
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("fadeIn");
-        } else {
-            if (!entry.target.classList.contains("animatedIn")) {
-                entry.target.classList.remove("fadeIn");
-            }
-        }
-    }) 
-}, {rootMargin: "0px 0px -15% 0px"})
-
-document.querySelectorAll([".animated", ".animatedIn"]).forEach((el) => observer.observe(el));
-// End of code from https://www.youtube.com/watch?v=T33NN_pPeNI
-
-
+// Before-after overlay
 const overlays = document.querySelectorAll(".before-after-overlay")
 if (document.body.contains(overlays[0])){
     function showBefore(e) {e.children[0].style.opacity = 0;}
     function hideBefore(e) {e.children[0].style.opacity = 1;}
     Array.from(overlays).forEach((overlay) => {
-        overlay.addEventListener("mouseover", event => {showBefore(overlay)})
-        overlay.addEventListener("mouseleave", event => {hideBefore(overlay)})
+        overlay.addEventListener("mouseover", () => {showBefore(overlay)})
+        overlay.addEventListener("mouseleave", () => {hideBefore(overlay)})
         overlay.addEventListener("touchstart", event => {
             showBefore(overlay)
             event.preventDefault();
@@ -272,3 +379,4 @@ if (document.body.contains(overlays[0])){
         })
     })
 }
+//////////
