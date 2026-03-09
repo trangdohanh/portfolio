cb365e8cef990bf18e8c5f5f371e06fdd78018d7
trangdohanh <125163105+trangdohanh@users.noreply.github.com>
2026-03-09 14:48:48 -0400

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -98,6 +98,11 @@ shoebox.innerHTML = `<div class="close-overlay" onclick="closeShoebox()"></div>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" width="24" height="24"><path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path></svg>
                     </button>
                     <div class="flair-container">
+						<div class="trapezoid-top"></div>
+                        <div class="trapezoid-left"></div>
+                        <div class="trapezoid-right"></div>
+                        <div class="trapezoid-bottom"></div>
+						
                         <p class="flair-text">(almost) everything I've worked on up to 2026</p>    
 
                         <div class="flair lg" style="bottom: 16.7%; right: 0;"><img src="img/shoebox/logo.png"></div>
